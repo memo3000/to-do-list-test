1a5b67e47b93d73770efa3cf493760b9abb1827b
JAEKYUNG KIM <memo1239@naver.com>
2023-11-16 08:14:16 +0900

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,18 +3,18 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <div className="flex">
-        <div className="flex mt-[38px] mb-[950px] ml-[64px] mr-[1228px]">
+      <div className="flex flex-row">
+        <div className="mt-[38px] mb-[950px] ml-[64px] mr-[1228px]">
           <Image
             src="/logo.png"
             width={148}
             height={36}
           />
         </div>
-        <div className="flex mt-[45px] mb-[955px] ml-[1146px] mr-[259px]">
+        <div className="mt-[45px] mb-[955px] ml-[1146px] mr-[259px]">
           blog
         </div>
-        <div className="flex mt-[39px] mb-[948px] ml-[1226px] mr-[64px]">
+        <div className="mt-[39px] mb-[948px] ml-[1226px] mr-[64px]">
           <Image
             src="/connect.png"
             width={150}
@@ -22,16 +22,18 @@ export default function Home() {
           />
         </div>
       </div>
-      <div>
-        {/* 왼쪽 내용 추가 */}
+      <div className="flex flex-col">
         <div>
-          <h1> Lorem Ipsum </h1>
-          <h2>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h2>
+          {/* 왼쪽 내용 추가 */}
+          <div>
+            <h1> Lorem Ipsum </h1>
+            <h2>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h2>
 
+          </div>
         </div>
-      </div>
-      <div>
-        {/* 오른쪽 내용 추가 */}
+        <div>
+          {/* 오른쪽 내용 추가 */}
+        </div>
       </div>
     </div>
   );
