938cf3b9590be620829aa37b618215dbeb5f1897
JAEKYUNG KIM <memo1239@naver.com>
2023-11-27 07:53:30 +0900

    231127

--- src/app/page.js
+++ src/app/page.js
@@ -2,26 +2,26 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-between h-screen">
+    <div className="flex flex-col">
       <div className="flex flex-row">
-        <div className="flex mt-15px ml-60px">
+        <div className="flex mt-15 ml-60 w-450 h-60 text-xl">
           Lineups & scores
         </div>
-        <div className="flex mt-30px ml-880px">
+        <div className="flex mt-30 ml-880 w-120 h-40 text-base">
           Lineups
         </div>
-        <div>
+        <div className="flex mt-30 ml-1030 w-100 h-40 text-base">
           Scores
         </div>
-        <div>
+        <div className="flex mt-30 ml-1150 w-260 h-40 text-base">
           Lineups & scores
         </div>
       </div>
-      <div className="flex justify-between">
-        <div className="w-1/2">
+      <div className="flex flex-row">
+        <div className="flex mt-100 ml-60 w-180 h-40 text-lg">
           leagues
         </div>
-        <div className="w-1/2">
+        <div className="flex">
           search
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontSize: {
+      sm: ['20px'],
+      base: ['24px'],
+      lg: ['30px'],
+      xl: ['40px'],
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
@@ -13,6 +19,33 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    margin: {
+      '15': '15px',
+      '30': '30px',
+      '60': '60px',
+      '100': '100px',
+      '880': '880px',
+      '1030': '1030px',
+      '1150': '1150px',
+    },
+    width: {
+      '450': '450px',
+      '120': '120px',
+      '100': '100px',
+      '260': '260px',
+      '180': '180px',
+      '450': '450px',
+      '450': '450px',
+    },
+    height: {
+      '60': '60px',
+      '40': '40px',
+      '450': '450px',
+      '450': '450px',
+      '450': '450px',
+      '450': '450px',
+      '450': '450px',
+    }
   },
   plugins: [],
 }
